d031a22d846ae6d07a11142983696d6cbd2a0b1f
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-06-12 11:20:43 -0400

    Finished most of the UI. Need to fix a bug where the final point is being displayed after the round is already over.

--- scripts.js
+++ scripts.js
@@ -6,10 +6,6 @@ function computerPlay(){
     return computerOptions[res];
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 let win = 0;
 let lose = 0;
 const cMove = document.querySelector(".cMove");
@@ -58,18 +54,23 @@ function round(playerS, computerS){
     }
 }
 
+function displayRoundRes(computerS){
 
-function displayRoundRes(playerS, computerS){
-
-    console.log("hello");
-    cMove.classList.add = "textRed";
+    computerScore.textContent = `Computer: ${lose}`;
+    youScore.textContent = `You: ${win}`;
     cMove.textContent = computerS;
 
 }
 
-
 function game(){
     const moveButton = document.querySelectorAll(".move");
+    const buttonImg = document.querySelectorAll("img");
+
+    buttonImg.forEach(function(img){
+        let move = img.getAttribute("alt");
+        img.addEventListener("mouseover", function(){this.src = `./media/${move}.gif`});
+        img.addEventListener("mouseleave", function(){this.src = "./media/still.png"});
+    });
 
     moveButton.forEach(div => div.addEventListener("click", () => {
         const playerMove = div.dataset.move;
@@ -79,15 +80,18 @@ function game(){
         displayRoundRes(playerMove, computerMove);
 
         if (win >= 5){
-            alert("You win!");
+            alert("You win! Press ok to play again.");
+            win = 0;
+            lose = 0;
+            displayRoundRes("...");
         }
         else if (lose >= 5){
-            alert("You lose.");
+            alert("You lose. Press ok to play again.");
+            win = 0;
+            lose = 0;
+            displayRoundRes("...");
         }
-        win = 0;
-        lose = 0;
     }));
 }
-
 game();
 
