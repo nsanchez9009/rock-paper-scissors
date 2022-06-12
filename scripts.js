3b30adcce7ddf39e70738161a86af5364959aa96
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-06-12 12:53:53 -0400

    added a sleep function in order to prevent the js from getting ahead of the html after updated. This fixed the bug of not showing the final round results. This was because the scoreCheck function was alerting before displayRoundRes could even update the html.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+let win = 0;
+let lose = 0;
+const cMove = document.querySelector(".cMove");
+const youScore = document.querySelector("#you");
+const computerScore = document.querySelector("#computer");
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function computerPlay(){
     const computerOptions = ["rock", "paper", "scissors"];
 
@@ -6,11 +16,27 @@ function computerPlay(){
     return computerOptions[res];
 }
 
-let win = 0;
-let lose = 0;
-const cMove = document.querySelector(".cMove");
-const youScore = document.querySelector("#you");
-const computerScore = document.querySelector("#computer");
+function scoreCheck(){
+    if (win == 5){
+        alert("You win! Press ok to play again.");
+        win = 0;
+        lose = 0;
+        displayRoundRes("...");
+    }
+    else if (lose == 5){
+        alert("You lose. Press ok to play again.");
+        win = 0;
+        lose = 0;
+        displayRoundRes("...");
+    }
+}
+
+function displayRoundRes(computerS){
+
+    cMove.textContent = computerS;
+    computerScore.textContent = `Computer: ${lose}`;
+    youScore.textContent = `You: ${win}`;
+}
 
 function round(playerS, computerS){
 
@@ -54,14 +80,6 @@ function round(playerS, computerS){
     }
 }
 
-function displayRoundRes(computerS){
-
-    computerScore.textContent = `Computer: ${lose}`;
-    youScore.textContent = `You: ${win}`;
-    cMove.textContent = computerS;
-
-}
-
 function game(){
     const moveButton = document.querySelectorAll(".move");
     const buttonImg = document.querySelectorAll("img");
@@ -72,26 +90,17 @@ function game(){
         img.addEventListener("mouseleave", function(){this.src = "./media/still.png"});
     });
 
-    moveButton.forEach(div => div.addEventListener("click", () => {
+    moveButton.forEach(div => div.addEventListener("click", async function(){
         const playerMove = div.dataset.move;
         const computerMove = computerPlay();
 
         round(playerMove, computerMove);
-        displayRoundRes(playerMove, computerMove);
+        displayRoundRes(computerMove);
+        await sleep(1000);
+        scoreCheck();
 
-        if (win >= 5){
-            alert("You win! Press ok to play again.");
-            win = 0;
-            lose = 0;
-            displayRoundRes("...");
-        }
-        else if (lose >= 5){
-            alert("You lose. Press ok to play again.");
-            win = 0;
-            lose = 0;
-            displayRoundRes("...");
-        }
     }));
 }
+
 game();
 
